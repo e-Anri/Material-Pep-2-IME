465e9d57b5d6c5cad99b0b0c58cd2b6977d99546
Stephan <stephan.silva@usach.cl>
2022-01-21 19:20:37 -0300

    [Commit 1] Subida de archivo respuesta y fuentes

---
+++ Desarrollo PEP 2/Forma-06.R
@@ -0,0 +1,56 @@
+# PEP 2 IME
+# Estefanía Alvarez (20.371.287-1)
+# Stephan Silva (20.298.778-8)
+
+# Pregunta 1
+
+# (23 puntos) Lord Vader desea saber si los niveles de exigencia con que los
+# distintos oficiales evaluadores (instructor, capitán, comandante y general) 
+# califican a los flametroopers son similares, por lo que le ha solicitado 
+# estudiar si existen diferencias significativas en el promedio de la 
+# evaluación realizada por cada uno de los oficiales. El Lord Sith ha sido muy 
+# claro al solicitar un reporte de aquellos oficiales cuyas evaluaciones
+# presenten diferencias.
+
+
+# Pregunta 2
+
+# (24 puntos) A fin de determinar si es necesario establecer programas de 
+# entrenamiento diferenciados para clones y reclutas, Lord Vader quiere saber 
+# si es posible distinguir entre ambas clases de soldados con los datos 
+# actuales. Para ello, ha solicitado evaluar un modelo clasificador que 
+# contemple entre 2 y 5 variables predictoras. Considere que, para ser 
+# aceptable, el modelo:
+
+# • Debe lograr una exactitud (accuracy) de al menos 0,8 en datos de prueba
+# • No puede considerar casos con demasiada influencia (considerando la 
+#   distancia de Cook)
+
+# • No debe presentar autocorrelación (usando la prueba de Durbin-Watson para
+#   un retardo y un nivel de significación α = .01)
+
+# • No debe presentar multicolinealidad severa (considerando el factor de 
+#   inflación de la varianza, con un VIF promedio inferior a 1,03).
+
+# Considere la semilla 1030 para obtener una muestra de 400 datos, 80% de los
+# cuales serán empleados para ajustar el modelo y el 20% restante, para 
+# evaluarlo.
+
+
+
+
+
+# Pregunta 3
+
+# (9 puntos) Proponga un ejemplo novedoso (no mencionado en clase ni que 
+# aparezca en las lecturas dadas) en donde un estudio o experimento, 
+# relacionado con el sentir de los santiaguinos ante el aumento de la 
+# violencia de la delincuencia, necesite utilizar una prueba de suma de 
+# rangos de Wilcoxon (también llamada prueba de Mann–Whitney–Wilcoxon o 
+# prueba U de Mann-Whitney) debido a problemas con la escala de la variable 
+# dependiente en estudio. Indiqué cuáles serían las variables involucradas 
+# en su ejemplo (con sus respectivos niveles) y las hipótesis nula y 
+# alternativa a contrastar.
+
+
+
